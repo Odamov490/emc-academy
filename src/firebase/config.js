55ea0e4022a693f2942ec7b1e94c2799d4d8598c
Odamov490 <Piskentems@gmail.com>
2026-04-14 17:01:27 +0500

    dx

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,15 @@
-// EMC Academy Firebase Config
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCkjaxRC2q3V1y58OvraEce9aDC_ptcLuE",
-  authDomain: "meetup-d67f6.firebaseapp.com",
-  projectId: "meetup-d67f6",
-  storageBucket: "meetup-d67f6.firebasestorage.app",
-  messagingSenderId: "942720343437",
-  appId: "1:942720343437:web:12d42e1360b65eaa23b1c4",
-  measurementId: "G-FNK8X9T5JC"
+  apiKey: "AIzaSyBU_hTOtzeJ7tkDUsTvWE6nBgtJMYWf2Cg",
+  authDomain: "emc-academy-e8e45.firebaseapp.com",
+  projectId: "emc-academy-e8e45",
+  storageBucket: "emc-academy-e8e45.firebasestorage.app",
+  messagingSenderId: "931383524380",
+  appId: "1:931383524380:web:4fd0831ed282026b1573d9",
+  measurementId: "G-4MSMLVXTM6"
 };
+
+const app = initializeApp(firebaseConfig);
+export default app;
